d819ba9dc11eb9fd36c5655e71ef30ddec68c9e3
Jimmy <jimmy.ganci@gmail.com>
2021-09-23 16:30:51 +0200

    change code js hover and click

--- index3.js
+++ index3.js
@@ -1,5 +1,11 @@
 const button = document.querySelectorAll("button");
 for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("mouseover", () => {
+    button[i].classList.add("hover");
+  });
+  button[i].addEventListener("mouseout", () => {
+    button[i].classList.remove("hover");
+  });
   button[i].addEventListener("mousedown", () => {
     button[i].classList.add("colorChange");
     console.log(button[i].value);
