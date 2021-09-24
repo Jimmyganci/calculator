0349273275b904b2f99ad8f7b1a272f3bb0aac08
Jimmy <jimmy.ganci@gmail.com>
2021-09-24 10:08:39 +0200

    logique eval

--- index3.js
+++ index3.js
@@ -1,4 +1,6 @@
 const button = document.querySelectorAll("button");
+const calcul = document.getElementById("numberInput");
+
 for (let i = 0; i < button.length; i++) {
   button[i].addEventListener("mouseover", () => {
     button[i].classList.add("hover");
@@ -6,11 +8,29 @@ for (let i = 0; i < button.length; i++) {
   button[i].addEventListener("mouseout", () => {
     button[i].classList.remove("hover");
   });
+
   button[i].addEventListener("mousedown", () => {
     button[i].classList.add("colorChange");
-    console.log(button[i].value);
+    if (button[i] !== "") {
+      let showNumber = [];
+      showNumber.push(button[i].value);
+      calcul.value += showNumber;
+      console.log(showNumber);
+    }
+    let number = calcul.value;
+    console.log(number);
   });
   button[i].addEventListener("mouseup", () => {
     button[i].classList.remove("colorChange");
   });
 }
+const total = document.getElementById("equal");
+total.addEventListener("click", () => {
+  calcul.value = eval(calcul.value);
+  if (calcul.value === "Infinity") {
+    calcul.value = 0;
+  }
+});
+document.getElementById("clear").addEventListener("click", () => {
+  calcul.value = "";
+});
